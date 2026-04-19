20f35713a6db1b35e19dd130c83de206d0651904
Pamindu-Rashmitha <pamidurashmitha@gmail.com>
2026-04-20 02:22:30 +0530

    Updated BASE_URL in axios configuration to use the production endpoint

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
-export const BASE_URL = 'http://192.168.1.8:5000';
+export const BASE_URL = 'https://rentgo-mwds.onrender.com';
 const API_BASE_URL = `${BASE_URL}/api`;
 
 const api = axios.create({
